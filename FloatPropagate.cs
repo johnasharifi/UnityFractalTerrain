2a4152f346ea9156d12755c1529806ab3bdd2dd0
jsharifi <johnasharifi@gmail.com>
2018-11-09 13:05:59 -0800

    opening repo

---
+++ FloatPropagate.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Threading;
+
+public static class FloatPropagate {
+	public static void ForcePropagate(ref float[,] map, float offset = 0.1f) {
+		int dim = map.GetLength (0);
+
+		bool repeat = true;
+		while (repeat) {
+			repeat = false;
+			for (int i = 0; i < dim; i++) {
+				for (int j = 0; j < dim; j++) {
+					if (map [i, j] == 0f) {
+						repeat = true;
+						int adj_i = (i + Random.Range (-1, 2) + dim) % dim;
+						int adj_j = (j + Random.Range (-1, 2) + dim) % dim;
+
+						map [i, j] = Mathf.Clamp01 (map [adj_i, adj_j] * Random.Range (1 - offset, 1 + offset));
+						}
+					}
+				}
+
+			}
+
+		}
+
+
+	public static void Blur(ref float[,] map) {
+		int dim = map.GetLength (0);
+
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				int adj_i = (i + Random.Range (-1, 2) + dim) % dim;
+				int adj_j = (j + Random.Range (-1, 2) + dim) % dim;
+
+				map [i, j] = (map [i, j] + map [adj_i, adj_j]) / 2f;
+				}
+			}
+		}
+
+	public static void Amplify(ref float[,] map) {
+		int dim = map.GetLength (0);
+		float[,] map_2x = new float[dim * 2, dim * 2];
+
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				map_2x [i * 2, j * 2] = map [i, j];
+				}
+			}
+		map = map_2x;
+		}
+
+	public static Texture2D HeightToTexture(ref float[,] map, Color[] color_lut) {
+		int dim = map.GetLength (0);
+		int hcount = color_lut.Length - 2;
+
+		Texture2D tex = new Texture2D (dim, dim);
+
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				int prev_index = (int) (map[i,j] * hcount);
+				int next_index = prev_index + 1;
+				Color c1_color = Color.Lerp(color_lut[prev_index], color_lut[next_index], (map[i,j] % (1f / hcount)) * hcount);
+
+				tex.SetPixel (i, j, c1_color);
+				}
+			}
+
+		tex.Apply ();
+		return(tex);
+		}
+
+	public static Texture2D TextureToNormal(ref Texture2D tex, float strength = 1f) {
+		Texture2D normalTexture;
+		float xLeft;
+		float xRight;
+		float yUp;
+		float yDown;
+		float yDelta;
+		float xDelta;
+
+		normalTexture = new Texture2D (tex.width, tex.height, TextureFormat.ARGB32, true);
+
+		for (int y=0; y<normalTexture.height; y++) 
+		{
+			for (int x=0; x<normalTexture.width; x++) 
+			{
+				xLeft = tex.GetPixel(x-1,y).grayscale*strength;
+				xRight = tex.GetPixel(x+1,y).grayscale*strength;
+				yUp = tex.GetPixel(x,y-1).grayscale*strength;
+				yDown = tex.GetPixel(x,y+1).grayscale*strength;
+				xDelta = ((xLeft-xRight)+1)*0.5f;
+				yDelta = ((yUp-yDown)+1)*0.5f;
+				normalTexture.SetPixel(x,y,new Color(xDelta,yDelta,1.0f,yDelta));
+				}
+			}
+		normalTexture.Apply();
+
+		//Code for exporting the image to assets folder
+		return normalTexture;
+		}
+
+	}
---
+++ ScriptTerrainAuto.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTerrainAuto : MonoBehaviour {
+	public Color[] color_lut;
+
+	private SplatPrototype[] splats;
+
+	private int twidth = 512;
+
+	private Terrain t;
+	private TerrainData tdata;
+
+	// Use this for initialization
+	void Start () {
+		Boot ();
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		}
+
+	void OnApplicationQuit() {
+		tdata.alphamapResolution = tdata.heightmapResolution = 32;
+		}
+
+	void Boot() {
+		t = GetComponent<Terrain> ();
+		tdata = t.terrainData;
+		tdata.splatPrototypes = SplatsFromColors (color_lut);
+		tdata.alphamapResolution = tdata.heightmapResolution = twidth;
+
+		float[,] heightmap = new float[twidth, twidth];
+		SeedHeightmap (ref heightmap);
+
+		for (int i = 0; i < 5; i++)
+		FloatPropagate.Blur (ref heightmap);
+
+		tdata.SetHeights (0, 0, heightmap);
+		tdata.SetAlphamaps (0, 0, SplatValuesFromTdata(ref tdata));
+		}
+
+	void SeedHeightmap(ref float[,] heightmap) {
+		int xdim = heightmap.GetLength (0);
+		int ydim = heightmap.GetLength (1);
+		int seed_count = 3;
+
+		for (int dum = 0; dum < seed_count; dum++) {
+			int xpos = Random.Range (0, xdim);
+			int ypos = Random.Range (0, ydim);
+
+			SpinDown (ref heightmap, xpos, ypos, 1f * dum / seed_count, 5, Random.Range (0f, Mathf.PI * 2f), xdim);
+			}
+
+		FloatPropagate.ForcePropagate (ref heightmap, offset: 0.05f);
+		for (int i = 0; i < 5; i++) {
+			FloatPropagate.Blur (ref heightmap);
+			}
+		}
+
+	void SpinDown(ref float[,] heightmap, int pos_x, int pos_y, float initial_value, int n, float theta, float r) {
+		if (n <= 1)
+			return;
+
+		int dim = heightmap.GetLength (0);
+
+		float inst_theta = theta;
+		float inst_value = initial_value;
+
+		for (int q = 3; q < n; q++) {
+			inst_theta = inst_theta + Random.Range (-0.2f, 0.2f);
+			inst_value = inst_value * 1f;
+
+			pos_x = (int) ((pos_x + r / n * Mathf.Cos (inst_theta) + dim) % dim);
+			pos_y = (int) ((pos_y + r / n * Mathf.Sin (inst_theta) + dim) % dim);
+
+			SpinDown (ref heightmap, pos_x, pos_y, inst_value, n - 1, inst_theta + Mathf.PI / 10, r * 0.5f);
+			SpinDown (ref heightmap, pos_x, pos_y, inst_value, n - 1, inst_theta - Mathf.PI / 10, r * 0.5f);
+
+			heightmap [pos_x, pos_y] = inst_value;
+			}
+
+		}
+
+	private SplatPrototype[] SplatsFromColors(Color[] colors) {
+		SplatPrototype[] splat_arr = new SplatPrototype[colors.Length];
+
+		for (int i = 0; i < colors.Length; i++) {
+			splat_arr [i] = new SplatPrototype ();
+			Texture2D tex = new Texture2D (1, 1);
+			tex.SetPixel (0, 0, colors [i]);
+			tex.Apply ();
+
+			splat_arr [i].texture = tex;
+			}
+
+		return(splat_arr);
+		}
+
+	private float[,,] SplatValuesFromTdata(ref TerrainData td) {
+		int dim = td.heightmapWidth - 1;
+		float[,] heightmap = td.GetHeights (0, 0, dim, dim);
+		float[,,] alpha_values = new float[dim, dim, td.alphamapLayers];
+
+		float denom = 1f / td.alphamapLayers;
+
+		float perl_coeff = 5f;
+
+		for (int i = 0; i < dim - 1; i++) {
+			for (int j = 0; j < dim - 1; j++) {
+				float a = Mathf.Clamp(heightmap [i, j] * td.alphamapLayers, 0, td.alphamapLayers);
+
+				int color_index = Mathf.Clamp ((int)a, 0, td.alphamapLayers - 1);
+				int color_index2 = Mathf.Clamp ((int)(a + 1), 0, td.alphamapLayers - 1);
+
+				float lerp_alpha = Mathf.Max(0.6f, a - Mathf.Floor (a));
+
+				alpha_values [i, j, color_index] = 1 - lerp_alpha;
+				alpha_values [i, j, color_index2] = lerp_alpha;
+
+				}
+			}
+
+		return(alpha_values);
+		}
+
+	
+	}
